eaa02f2a7e7b902180e356cb005cafdd3123c1d6
Mario Sebastian Navarro <senix55@gmail.com>
2025-03-31 21:37:18 -0300

    Realizado punto 1, cargado los demás puntos a realizar.

--- TPLog-CJNY- EshtabaEnLaFloreria.pl
+++ TPLog-CJNY- EshtabaEnLaFloreria.pl
@@ -60,3 +60,22 @@ plantas_companeras(rose, bamboo).
 
 
 % A partir de acá, crear las reglas para cumplir con los puntos pedidos en el tp.
+% 1.Queremos poder preguntar sobre nuestras plantas si: a. Son de tipo arbusto. b. Florecen en primavera. c. Tienen un color específico.
+
+esArbusto(Planta):-planta(Planta,tipo(arbusto)).
+florecenEnPrimavera(Planta):-planta(Planta,epoca(floracion,primavera)).
+colorEspecifico(Planta,Color):-planta(Planta,color(Color)).
+
+% 2.Sabemos que: a. Las plantas que son arbustos y florecen en verano deben tener un sistema de riego especial. b. Las plantas rojas o amarillas atraen más insectos benéficos. c. Las plantas que no son de tipo flor son consideradas altas.
+
+% Realizar el predicado que nos permita agregar este conocimiento y el punto 1 no deba modificarse.
+
+% 3.Se necesita conocer el conjunto de todas las plantas que son cortas y de tipo flor.
+
+% 4.Ahora debemos agregar las pistas obtenidas durante las observaciones. Por ejemplo:
+
+% pista(arbol_rojo, tipo(arbusto)).
+% pista(arbol_rojo, altura(media)).
+% Queremos relacionar una planta y un observador solo si todas las pistas que tiene el observador son características de la planta. En este punto no se puede usar findall.
+
+% 5.Finalmente, queremos saber si una planta está atrayendo más visitas que su compañera. Para ello, la cantidad de pistas que cumple debe ser mayor que la de las plantas compañeras.
